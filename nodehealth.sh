2fbe59792b90cc0d2a911e5d4ce64c174cefcccc
Tarun-Chand-Illapu <tarun.chand.31@gmail.com>
2024-01-19 00:15:38 -0500

    First commit

--- nodehealth.sh
+++ nodehealth.sh
@@ -1,2 +1,34 @@
- df -h
- free -g
+#!/bin/bash
+
+##############################
+#
+# Author: Tarun Chand Illapu
+# Date: 01/17/2024
+#
+# This Script outputs the Node Health
+#
+# Version: V1
+#
+##############################
+
+set -x # Debug Mode
+set -e # Exit the script when there is an Error
+set -o pipefail 
+
+echo " "
+echo "Amount of Disk Space Avaiable"
+echo " "
+df -h
+echo " "
+echo "Ram Usage "
+echo " "
+free -g
+echo " "
+echo "Number of Processing Units"
+echo " "
+nproc
+
+echo "PID of the user Processes"
+echo " "
+ps -ef | grep user |awk -F" " '{print $2}' 
+
